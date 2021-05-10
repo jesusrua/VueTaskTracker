1ef21e70e21787c49dfdcda8eee801cb30cc51b6
Jesus Rua <jesus.rua@patterson.agency>
2021-05-10 17:57:47 +0200

    second commit

---
+++ src/components/Header.vue
@@ -0,0 +1,35 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+    </header>
+    <Button 
+        @toggle-add-task="$emit('toggle-add-task')" 
+        :text="showAddTask ? 'Close' : 'Add Task'" 
+        color="Green"
+    />
+</template>
+
+<script>
+
+    import Button from './Button'
+
+    export default {
+        name: 'Header',
+        props: {
+            title: String,
+            showAddTask: Boolean
+        },
+        components: {
+            Button
+        }
+    }
+</script>
+
+<style scoped>
+    header {
+        display:flex;
+        justify-content: space-between;
+        align-items: center;
+        margin: 20px;
+    }
+</style>
